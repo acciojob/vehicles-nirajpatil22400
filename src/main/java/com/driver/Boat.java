26d4a62fac4e20a32b9716a68835ad491f993155
NirajPatil <nirajpatil@22400.com>
2023-02-11 13:35:36 +0530

    class boat created

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -6,6 +6,27 @@ public class Boat implements WaterVehicle {
 
     private int capacity;
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Boat(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
     @Override
     public int getVehicleCapacity() {
         // TODO Auto-generated method stub
